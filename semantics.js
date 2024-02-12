61bf1129bb0bd6bda73ad2312107b862fd88bee5
Arthur <58260630+artoer@users.noreply.github.com>
2024-02-12 11:05:15 -0500

    bring back dribbble

--- semantics.js
+++ semantics.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const footerTemplate = Handlebars.compile(`
   <div>
   <a href="https://www.linkedin.com/in/arthur-menken/" title="LinkedIn" target="_blank"><i class="fa-brands fa-linkedin-in fa-2xl"></i></a>
+  <a href="https://dribbble.com/artioer" title="Dribbble" target="_blank"><i class="fa-brands fa-dribbble fa-2xl"></i></a>
   <a href="mailto:arthur_menken@hotmail.com" title="Email"><i class="fa-regular fa-at fa-2xl"></i></a>
   </div>
     <span>&copy; 2024</span>
@@ -22,4 +23,3 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector('nav').innerHTML = navTemplate();
 });
 
-/* <a href="https://dribbble.com/artioer" title="Dribbble" target="_blank"><i class="fa-brands fa-dribbble fa-2xl"></i></a> */
